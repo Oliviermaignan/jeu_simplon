cbb3dcffc0d2ffac3631a279553bb7acef4ac435
Olivier <ol.maignan@gmail.com>
2024-02-06 16:03:11 +0100

    les barres marchent avec des classes

--- ProgressBar.js
+++ ProgressBar.js
@@ -1,9 +1,15 @@
 export class ProgressBar {
     #title;
+    #id;
 
-    constructor (title){
+    constructor (title, id){
         this.title = title;
-
+        this.id = id;
+        this.createHTML()
+        this.displayTitle();
+        this.changeColor('green');
+        this.setProgressBar();
+        this.decreasingProgress();
     };
 
     get title(){
@@ -11,33 +17,61 @@ export class ProgressBar {
     }
 
     set title(title){
-
         this.#title= title;
+    }
+
+    get id(){
+        return this.#id
+    }
+
+    set id(id){
+        this.#id= id;
+    }
+
+    createHTML(){
+        let progressBarContainer = document.querySelector('.progress-bar-container')
+
+        let thisBarContainer = document.createElement('div');
+        thisBarContainer.classList.add(`${this.title}-bar-container`);
+
+        let progressBarTitle = document.createElement('p');
+        progressBarTitle.classList.add(`${this.title}-bar-title`);
+
+        let progressBar = document.createElement('div');
+        progressBar.classList.add('progress-bar');
+
+        let progress = document.createElement('div');
+        progress.classList.add('progress');
+        progress.setAttribute('id', this.id);
+
+        progressBarContainer.append(thisBarContainer);
+        thisBarContainer.append(progressBarTitle, progressBar)
+        progressBar.append(progress);
     }
 
     displayTitle(){
         let title = this.title;
-        let titleElement = document.querySelector('#faimBarTitle');
+        let titleElement = document.querySelector(`.${title}-bar-title`);
         titleElement.textContent = title;
     }
 
     changeColor (color){
-        let progressBarElement = document.querySelector('#faimBar');
+        console.log(this.id)
+        let progressBarElement = document.querySelector(`#${this.id}`);
         progressBarElement.style.backgroundColor = color;
     }
 
     setProgressBar(){
-        let progressBarElement = document.querySelector('#faimBar');
+        let progressBarElement = document.querySelector(`#${this.id}`);
         progressBarElement.style.width= '100%';
     }
 
     decreasingProgress(){
         let width = 100;
-        let progressElements = document.querySelectorAll('.progress')
+        let progressElements = document.querySelectorAll(`#${this.id}`)
         let time = setInterval(()=>{
-            console.log(width);
             if (width === 0){
-                console.log('dead')
+                this.displayDead()
                 clearInterval(time)
             }
             if (width > 40){
@@ -53,10 +87,15 @@ export class ProgressBar {
                     element.style.width = width + '%';
                 })
             }
-        },1)
+        },500)
     }
 
     displayDead(){
-
+        console.log('dead')
     }
+
+    increasingBar(){
+        
+    }
+
 }
--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 import { ProgressBar } from "./ProgressBar.js"; 
 
-let lifeBar = new ProgressBar('faim');
-lifeBar.changeColor ('green')
-lifeBar.setProgressBar()
-lifeBar.decreasingProgress()
-console.log(lifeBar)
+let lifeBar = new ProgressBar('faim', 'faimBar');
+let joieBar = new ProgressBar('joie', 'joieBar');
+let sortieBar = new ProgressBar('sortie', 'sortieBar');
+let dodoBar = new ProgressBar('dodo', 'dodoBar');
