db62b86fb245d8ebfaa5f041add5883ecb0acd1d
Olivier <ol.maignan@gmail.com>
2024-02-09 15:17:15 +0100

    quasi fin mode outdoor

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ let joieBar = new ProgressBar('joie', 'joieBar');
 let dodoBar = new ProgressBar('dodo', 'dodoBar');
 let sortieBar = new ProgressBar('sortie', 'sortieBar');
 
-
-
 let faimBtn = document.querySelector('#faimIcon')
 faimBtn.addEventListener('click', ()=>{
     lifeBar.addLife();
@@ -28,5 +26,37 @@ sortieBtn.addEventListener('click', ()=>{
     sortieBar.sourir();
 })
 
-document.addEventListener('')
+
+animation()
+
+function animation(){
+    keyDown();
+    removeAnimation();
+}
+
+function keyDown(){
+    let pivoine = document.querySelector(".tama-outdoor")
+    if (document.body.classList.contains('outdoor-body')){
+        document.addEventListener('keydown', (e)=>{
+            pivoine.style.animation = 'marche infinite 1s';
+            if(e.key==='ArrowLeft'){
+                pivoine.style.left = (pivoine.computedStyleMap().get('left').value - 5)+'px' ;
+                pivoine.style.transform= 'scaleX(1)';
+            };
+            if(e.key==='ArrowRight'){
+                pivoine.style.left = (pivoine.computedStyleMap().get('left').value + 5)+'px' ;
+                pivoine.style.transform= 'scaleX(-1)';
+            };
+        })
+    }
+}
+
+function removeAnimation(){
+    let pivoine = document.querySelector(".tama-outdoor")
+    if (document.body.classList.contains('outdoor-body')){
+        document.addEventListener('keyup', (e)=>{
+            pivoine.style.animation = 'none';
+        })
+    }
+}
 
